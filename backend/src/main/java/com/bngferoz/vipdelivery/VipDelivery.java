0dcb3ce8e26c4af3c67796cea595920db4c58b44
bngferoz <bngferoz@gmail.com>
2024-03-26 17:19:28 +0600

    spring boot connected with mysql

---
+++ backend/src/main/java/com/bngferoz/vipdelivery/VipDelivery.java
@@ -0,0 +1,13 @@
+package com.bngferoz.vipdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VipDelivery {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VipDelivery.class, args);
+	}
+
+}
---
+++ backend/src/main/java/com/bngferoz/vipdelivery/dao/ProductCategoryRepository.java
@@ -0,0 +1,12 @@
+package com.bngferoz.vipdelivery.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import com.bngferoz.vipdelivery.entity.ProductCategory;
+
+@CrossOrigin("http://localhost:4200")
+@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+}
